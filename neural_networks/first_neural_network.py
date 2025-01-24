5a78fd711122163533af4874b84fc8d4548f7183
karishma-battina <67629745+karishma-battina@users.noreply.github.com>
2025-01-24 02:14:13 +0000

    axis and keepdims

--- neural_networks/first_neural_network.py
+++ neural_networks/first_neural_network.py
@@ -212,3 +212,22 @@ dense1.forward(X)
 
 # Let's see output of the first few samples:
 print(dense1.output[:5])
+
+#softmax
+A = [[1, 2, 3], [4, 5, 6], [7, 8,9]]
+print(np.sum(A))
+
+print(np.sum(A, axis = 0))
+print(np.sum(A, axis = 0).shape)
+
+print(np.sum(A, axis = 1))
+print(np.sum(A, axis = 1).shape)
+
+print(np.sum(A, axis = 0,keepdims = True))
+print(np.sum(A, axis = 0,keepdims = True).shape)
+
+print(np.sum(A, axis = 1,keepdims = True))
+print(np.sum(A, axis = 1,keepdims = True).shape)
+
+print(np.max(A, axis = 0))
+print(np.max(A, axis = 1))
